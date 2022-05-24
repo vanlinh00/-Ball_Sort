978d1d935227a696b7b6a43ddfb76e567a7c8882
vanlinh00 <vanlinh20192019@gmail.com>
2022-05-25 01:33:58 +0700

    await 1s and particle add

---
+++ Assets/Scripts/ClickAction.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickAction : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // OnClick code goes here ...
+        Debug.Log("hi");
+    }
+
+}
---
+++ Assets/Scripts/UiController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UiController : MonoBehaviour
+{
+    public Text textCoin;
+    int addCoinInt = 0;
+    public static UiController Instance;
+    public GameObject btStartGame;
+    public GameObject allCavas;
+
+    [SerializeField]
+    GameObject _complete;
+    void Start()
+    {
+        Instance = this;
+
+        DontDestroyOnLoad(this.gameObject);
+        DontDestroyOnLoad(allCavas);
+    }
+    void Update()
+    {
+
+    }
+    public void ButtonCheck()
+    {
+        GameController.Instance.CheckRanDom();
+    }
+    public void startGame()
+    {
+
+
+        btStartGame.SetActive(false);
+        _complete.SetActive(false);
+        SceneManager.LoadScene(1);
+    }
+
+    public void nextLevel()
+    {
+        GameController.Instance._setUpGame.SetActive(false);
+
+        _complete.SetActive(true);
+
+    }
+
+    public void addCoin()
+    {
+        addCoinInt += 200;
+        textCoin.text = addCoinInt.ToString();
+    }
+
+}
